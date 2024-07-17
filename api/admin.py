08f0f002045467eb83ba983b1d52c3f9bb63c085
5ekastan <151753486+nasirovx@users.noreply.github.com>
2024-07-17 08:33:00 +0300

    Generic api + docker 🦄

--- api/admin.py
+++ api/admin.py
@@ -3,6 +3,6 @@ from .models import Contacts
 
 @admin.register(Contacts)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = ['name', 'email', 'phone']
+    list_display = ['name', 'email', 'phone'] 
     list_filter = ['name', 'email', 'phone']
     list_fields = ['name', 'email', 'phone'] 
